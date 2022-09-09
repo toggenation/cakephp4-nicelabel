c6b1386808fb555dc21875619d2e8c57f50e6d08
James McDonald <james@toggen.com.au>
2022-09-09 06:24:51 +0000

    Added http method

--- src/Controller/PrintingController.php
+++ src/Controller/PrintingController.php
@@ -4,6 +4,13 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Utility\LabelFactory;
+use Cake\Chronos\Chronos;
+use Cake\Http\Client;
+use Cake\Network\Socket;
+use Cake\Routing\Router;
+use Cake\View\ViewBuilder;
+use CsvView\View\CsvView;
 use Faker\Factory;
 
 /**
@@ -12,19 +19,77 @@ use Faker\Factory;
  */
 class PrintingController extends AppController
 {
-    public function label()
+    public function socket()
     {
-        // code ;
+        [$header, $data] = (new LabelFactory())->make();
 
-        $faker = Factory::create();
-        /**
-         * @var \Faker\Provider\FakeBarcode
-         */
-        $faker->addProvider(new \Faker\Provider\FakeBarcode($faker));
+        $vb = new ViewBuilder();
 
-        dd([
-            $faker->sscc(3),
-            $faker->gtin14()
-        ]);
+        $vb->setClassName('CsvView.Csv')
+            ->setOptions([
+                'serialize' => 'data',
+                'header' => $header
+            ]);
+
+        $view = $vb->build();
+
+        $view->set(compact('data'));
+
+        $send = $view->render();
+
+        $url = 'tcp://10.197.3.140:11973';
+
+        $config = parse_url($url);
+
+        $socket = new Socket($config);
+
+        $socket->setConfig('timeout', 1);
+
+        try {
+            $socket->connect();
+        } catch (\Cake\Network\Exception\SocketException $e) {
+            return $this->getResponse()
+                ->withStringBody($e->getMessage())
+                ->withType('text');
+        }
+
+        $socket->write($send);
+
+        $socket->disconnect();
+
+        return $this->getResponse()
+            ->withStringBody("sent to $url\n\n" . $send)
+            ->withType('text');
+    }
+
+    public function http()
+    {
+
+        [$header, $data] = (new LabelFactory())->make(1);
+
+        $vb = new ViewBuilder();
+
+        $vb->setClassName('Xml')
+            ->setOptions([
+                // 'rootNode' => 'hiJames',
+                'serialize' => 'data'
+            ]);
+
+        $view = $vb->build();
+
+        $view->set(compact('data'));
+
+        $send = $view->render();
+
+        $url = 'http://10.197.3.140:56425/palletPrint';
+
+
+        $client = new Client();
+
+        $response = $client->post($url, $send, ['type' => 'xml']);
+
+        return $this->getResponse()
+            ->withStringBody($response->getXml()->asXML())
+            ->withType('xml');
     }
 }
